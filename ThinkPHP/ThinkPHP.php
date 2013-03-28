8cfecb20b3ebcf92096d1453aae8c2ba97e166c0
mos <moswh07@gmail.com>
2013-03-28 14:30:55 +0800

    不需要引擎拓展

--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -20,17 +20,13 @@ defined('THINK_PATH') 	or define('THINK_PATH', dirname(__FILE__).'/');
 defined('SYSTEM_PATH') 	or define('SYSTEM_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
 defined('RUNTIME_PATH') or define('RUNTIME_PATH',realpath(SYSTEM_PATH).'/Runtime/');
 defined('SYSTEM_DEBUG') 	or define('SYSTEM_DEBUG',false); // 是否调试模式
-if(defined('ENGINE_NAME')) {
-    defined('ENGINE_PATH') or define('ENGINE_PATH',THINK_PATH.'Extend/Engine/');
-	require ENGINE_PATH.strtolower(ENGINE_NAME).'.php';
+
+$runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
+defined('RUNTIME_FILE') or define('RUNTIME_FILE',RUNTIME_PATH.$runtime);
+if(!SYSTEM_DEBUG && is_file(RUNTIME_FILE)) {
+	// 部署模式直接载入运行缓存
+	require RUNTIME_FILE;
 }else{
-	$runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
-	defined('RUNTIME_FILE') or define('RUNTIME_FILE',RUNTIME_PATH.$runtime);
-	if(!SYSTEM_DEBUG && is_file(RUNTIME_FILE)) {
-	    // 部署模式直接载入运行缓存
-	    require RUNTIME_FILE;
-	}else{
-	    // 加载运行时文件
-	    require THINK_PATH.'Common/runtime.php';
-	}	
-}
+	// 加载运行时文件
+	require THINK_PATH.'Common/runtime.php';
+}	
